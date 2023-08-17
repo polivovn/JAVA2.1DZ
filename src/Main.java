603120528ed8fc4fca8eb8c753304e109ebf2b90
polivovn <139216581+polivovn@users.noreply.github.com>
2023-08-17 16:52:09 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int spent = 20;
         int mile = 1;
-        int ticket_price = 96_840;
-        int bonus = ticket_price / spent;
+        int ticketPrice = 96_840;
+        int bonus = ticketPrice / spent;
         System.out.println("Итоговый бонус: " + bonus);
     }
 }
